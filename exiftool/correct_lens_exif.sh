e798b773b2b894e0328d69853c5dd41c9a48a957
tweckert <lavertezzo@gmx.de>
2016-09-29 15:21:07 +0200

    Added a comment about what this script actually does.

--- exiftool/correct_lens_exif.sh
+++ exiftool/correct_lens_exif.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
+# Apple Aperture seems to use the sometimes incomplete 'Lens' meta data tag to display the lens data.
+# However, some cameras store the lens information in the 'LensID' meta data tag.
+# This exiftool call copies the content from the 'LensID' to the 'Lens' meta data tag.
 exiftool -overwrite_original_in_place -progress -r -ext jpg -ext tif -ext dng -P -tagsfromfile @ '-Lens<LensID' .
